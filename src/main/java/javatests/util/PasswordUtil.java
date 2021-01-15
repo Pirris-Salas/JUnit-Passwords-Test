b154aa52c6edbcf7a9cd588730a3267d8dae5bd8
Pirris <luissalas_10@outlook.com>
2021-01-14 21:04:00 -0600

    Proyecto en el que creamos test unitarios para definir si una contraseña es´WEAK MEDIUM o STRONG

---
+++ src/main/java/javatests/util/PasswordUtil.java
@@ -0,0 +1,28 @@
+package javatests.util;
+
+public class PasswordUtil {
+
+    public enum SecurityLevel {
+        WEAK,MEDIUM,STRONG
+    }
+
+    public static SecurityLevel assessPassword(String password){
+
+        if(password.length() < 8){
+            return SecurityLevel.WEAK;
+        }
+
+        //a-z contiene letras minusculas de la a a la z
+        //A-Z contiene letras mayusculas de la A a la Z
+        //+ significa que se pueden repetir las letras
+        if(password.matches("[a-zA-Z]+")){
+            return SecurityLevel.WEAK;
+        }
+
+        if(password.matches("[a-zA-Z0-9]+")){
+            return SecurityLevel.MEDIUM;
+        }
+
+        return SecurityLevel.STRONG;
+    }
+}
