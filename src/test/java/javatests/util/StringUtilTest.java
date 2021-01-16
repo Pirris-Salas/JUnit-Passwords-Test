444127743ac67b6512efa87ea5dfbe76ae9a4426
Pirris <luissalas_10@outlook.com>
2021-01-15 20:55:20 -0600

    Se crean varios test, entre ellos  uno que arroja una excepción para cuando se ingresen números negativos

--- src/test/java/javatests/util/StringUtilTest.java
+++ src/test/java/javatests/util/StringUtilTest.java
@@ -6,9 +6,26 @@ import org.junit.Test;
 public class StringUtilTest {
 
     @Test
-    public void testRepeat(){
+    public void repeat_string_once(){
+
+        Assert.assertEquals("Salas, ", StringUtil.repeat("Salas, ", 1));
+    }
+
+    @Test
+    public void repeat_string_multiple_times(){
 
         Assert.assertEquals("Luis, Luis, Luis, ", StringUtil.repeat("Luis, ", 3));
-        Assert.assertEquals("Salas, ", StringUtil.repeat("Salas, ", 1));
+    }
+
+    @Test
+    public void repeat_string_zero_times(){
+
+        Assert.assertEquals("", StringUtil.repeat("Luis, ", 0));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void repeat_string_negative_times(){
+
+        StringUtil.repeat("Luis, ", -1);
     }
 }
