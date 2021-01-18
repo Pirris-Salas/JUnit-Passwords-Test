e47e8e7fe8397da72e82ff799ce3f27d8c923748
Pirris <luissalas_10@outlook.com>
2021-01-17 20:09:16 -0600

    Utilizando Test Driven Development se crea en código para saber si un año es bisiesto al crear primeramente los test

---
+++ src/main/java/javatests/util/util/DateUtil.java
@@ -0,0 +1,10 @@
+package javatests.util.util;
+
+public class DateUtil {
+
+    public static boolean isLeapYear(int year){
+
+        return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+    }
+
+}
